8d791c45369af427c849ee91683c69ac389ddc3c
arsobhan <53423272+arsobhan@users.noreply.github.com>
2020-10-27 00:22:21 +0530

    Add files via upload

---
+++ gateway-edge-server/src/main/java/com/sample/gatewayedgeserver/GatewayEdgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.sample.gatewayedgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayEdgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayEdgeServerApplication.class, args);
+	}
+
+}
